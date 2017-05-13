b3bda1ce4fc15c19fb9843f21bd71336d9ffe7eb
Roman Rodionov <rodionov.rn@yandex.ru>
2017-05-13 19:04:03 +0300

    Isotope masses and isotope natural abundance.

--- mckit/constants.py
+++ mckit/constants.py
@@ -24,4 +24,30 @@ IDENTITY_ROTATION = np.eye(3)
 # angle tolerance
 ANGLE_TOLERANCE = 0.001
 
+# Resolution of float number
 RESOLUTION = np.finfo(float).resolution
+
+# Natural presence of isotopes
+ATOM_NAMES = {}
+NATURAL_ABUNDANCE = {}
+ISOTOPE_MASS = {}
+
+with open('data/isotopes.dat') as f:
+    for line in f:
+        number, name, *data = line.split()
+        number = int(number)
+        ATOM_NAMES[name] = number
+        NATURAL_ABUNDANCE[number] = {}
+        ISOTOPE_MASS[number] = {}
+        for i in range(len(data) // 3):
+            isotope = int(data[i * 3])
+            ISOTOPE_MASS[number][isotope] = float(data[i * 3 + 1])
+            abun = data[i * 3 + 2]
+            if abun != '*':
+                NATURAL_ABUNDANCE[number][isotope] = float(abun) / 100.0
+
+if __name__ == '__main__':
+    print(ATOM_NAMES)
+    print(ISOTOPE_MASS)
+    print(NATURAL_ABUNDANCE)
+
