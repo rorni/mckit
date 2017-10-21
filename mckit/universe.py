de94bc3c94ec6fbc83484d5489d8e68bc67162ea
Roman Rodionov <rodionov.rn@yandex.ru>
2017-10-21 21:12:02 +0300

    Universe class skeleton.

--- mckit/universe.py
+++ mckit/universe.py
@@ -14,6 +14,12 @@ class Universe:
         
     Methods
     -------
+    get_volumes(cell_names)
+        Calculates volumes of cells.
+    get_concentrations()
+        Calculates concentrations of materials in voxels.
+    populate()
+        Populates every cell that has fill option by cells of filling universe.
     transform(tr)
         Applies transformation tr to this universe. Returns a new universe.
     """
@@ -21,6 +27,55 @@ class Universe:
         self.cells = tuple(cells)
         self.name = name
         self.description = description
+
+    def get_volumes(self, cell_names=None):
+        """Calculates volumes of cells.
+
+        Parameters
+        ----------
+        cell_names : list[int]
+            Names of cells which volumes are to be calculated.
+
+        Returns
+        -------
+        volumes : dict or list
+            Volumes of cells
+        """
+        raise NotImplementedError
+
+    def get_concentrations(self, mesh):
+        """Calculates concentrations of each material in mesh.
+
+        Parameters
+        ----------
+        mesh : Mesh
+            Mesh object.
+
+        Returns
+        -------
+        concentrations : dict
+            Concentrations of materials for mesh.
+        """
+        raise NotImplementedError
+
+    def populate(self):
+        """Replaces cells that have fill option by filling universe cells.
+
+        This procedure is applied recursively. The resulting universe does not
+        contain cells that have fill option.
+
+        Returns
+        -------
+        new_universe : Universe
+            New universe that has no inner universes.
+        """
+        new_cells = []
+        for c in self.cells:
+            if 'FILL' in c.options.keys():
+                new_cells += c.populate()
+            else:
+                new_cells.append(c)
+        return Universe(new_cells)
 
     def transform(self, tr):
         tr_cells = []
