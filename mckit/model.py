f0d6dc26e141a772cf224cc71147e393910a1715
Roman Rodionov <r.rodionov@iterrf.ru>
2017-10-25 19:20:41 +0300

    extract_submodel and list_universes methods for Model class are implemented and tested.

--- mckit/model.py
+++ mckit/model.py
@@ -15,7 +15,7 @@ from .material import Material
 from .constants import RELATIVE_DENSITY_TOLERANCE
 
 
-def read_mcnp_model(filename):
+def read_mcnp(filename):
     """Reads MCNP model from file and creates corresponding objects.
 
     Parameters
@@ -33,118 +33,6 @@ def read_mcnp_model(filename):
     lexer.begin('INITIAL')
     title, cells, surfaces, data = parser.parse(text)
     return Model(title, cells, surfaces, data)
-
-
-def _replace_geometry_names_by_objects(cells, surfaces):
-    """Replaces surface and cell names in geometry description by objects.
-
-    Parameters
-    ----------
-    cells : dict
-        Dictionary of cell parameters.
-    surfaces : dict
-        Dictionary of surface objects
-    """
-    unhandled_cells = deque(cells.keys())
-    while unhandled_cells:
-        name = unhandled_cells.popleft()
-        if 'reference' in cells[name].keys():
-            ref_cell = cells[name]['reference']
-            if ref_cell in unhandled_cells:
-                unhandled_cells.append(name)
-            else:
-                cells[name]['geometry'] = cells[ref_cell]['geometry'].copy()
-        else:
-            geom = cells[name]['geometry']
-            for i in reversed(range(len(geom))):
-                if geom[i] == '#':
-                    comp_cell = geom[i-1]
-                    if comp_cell in unhandled_cells:
-                        unhandled_cells.append(name)
-                        break
-                    geom[i-1:i+1] = cells[comp_cell]['geometry'] + ['C']
-                elif isinstance(geom[i], int):
-                    geom[i] = surfaces[geom[i]]
-
-
-def _create_material_objects(cells, compositions):
-    """Creates material objects and assign them to cells.
-
-    Materials objects are assigned to 'material' keyword of cell.
-
-    Parameters
-    ----------
-    cells : dict
-        Dictionary of cell parameters.
-    compositions : dict
-        Dictionary of material parameters, that define material composition.
-        Its values are also dictionaries with 'wgt' or 'atomic' keywords.
-        
-    Returns
-    -------
-    materials : dict
-        A dictionary of created material objects. Keys - composition numbers, 
-        values - lists of corresponding Material instances (they differ only in
-        density). 
-    """
-    created_materials = {}  # Stores materials created in dictionary
-                            # composition_name -> material_instance
-    for cell in cells.values():
-        if 'MAT' in cell.keys():
-            comp_name = cell['MAT']
-            density = cell['RHO']
-            # Check if material already exists.
-            if comp_name in created_materials.keys():
-                mat = _get_material(created_materials[comp_name], density)
-            else:
-                mat = None
-                created_materials[comp_name] = []
-
-            # Create new material
-            if not mat:
-                mat_params = {}
-                if density > 0:
-                    mat_params['concentration'] = density
-                elif density < 0:
-                    mat_params['density'] = abs(density)
-                if 'atomic' in compositions[comp_name].keys():
-                    mat_params['atomic'] = compositions[comp_name]['atomic']
-                if 'wgt' in compositions[comp_name].keys():
-                    mat_params['wgt'] = compositions[comp_name]['wgt']
-                mat = Material(**mat_params)
-                created_materials[comp_name].append(mat)
-            cell['material'] = mat
-    return created_materials
-
-
-def _get_material(materials, density):
-    """Checks if material with specified density already exists and returns it.
-
-    Parameters
-    ----------
-    materials : list[Material]
-        List of materials with the same composition.
-    density : float
-        Density of material. If positive, then it is concentration; if negative,
-        it is weight density.
-
-    Returns
-    -------
-    material : Material
-        If material with specified density already exists, it is returned.
-        Otherwise None is returned.
-    """
-    if density < 0:
-        method = Material.density
-        density = abs(density)
-    else:
-        method = Material.concentration
-    for mat in materials:
-        mat_den = method(mat)
-        rel = 2 * abs(mat_den - density) / (mat_den + density)
-        if rel < RELATIVE_DENSITY_TOLERANCE:
-            return mat
-    return None
 
 
 class Model:
@@ -167,24 +55,77 @@ class Model:
         A name of base universe for model. Cells of this universe will have
         parameter U=0. The model will contain only cells that belong to
         inner universes of this one.
+    take_inserted : bool
+        Whether to take cells inserted by fill operations. If False, no filling
+        universes are taken, fill options are moved in comments. Default True.
         
     Methods
     -------
-    get_universe_model(uname, title)
+    extract_submodel(uname, title, take_inserted)
         Gets the Model instance that corresponds to the specified universe.
-    get_universe_list()
+    list_universes()
         Gets the list of universe names.
     universe()
         Gets the model in object representation (as Universe instance).
     save(filename)
         Saves model.
     """
-    def __init__(self, title, cells, surfaces, data, base_universe=0):
+    def __init__(self, title, cells, surfaces, data, base_universe=0,
+                 take_inserted=True):
         self.title = deepcopy(title)
-        self.cells = cells
-        self.surfaces = surfaces
-        self.data = data
+        self.cells = _get_contained_cells(cells, base_universe, take_inserted)
+        surf_ind = _get_surface_indices(self.cells)
+        self.surfaces = {si: deepcopy(surfaces[si]) for si in surf_ind}
+        self.data = deepcopy(data)
+        self.data.pop('M', None)
+        self.data.pop('TR', None)
+        mat_ind = _get_composition_indices(self.cells)
+        tr_ind = _get_transformation_indices(self.cells, self.surfaces)
+        if mat_ind:
+            self.data['M'] = {mi: deepcopy(data['M'][mi]) for mi in mat_ind}
+        if tr_ind:
+            self.data['TR'] = {ti: deepcopy(data['TR'][ti]) for ti in tr_ind}
 
+    def save(self, filename):
+        raise NotImplementedError
+
+    def list_universes(self):
+        """Gets the list of universe names.
+
+        Returns
+        -------
+        unames : list[int]
+            List of included universe names.
+        """
+        unames = set()
+        for cell_data in self.cells.values():
+            if 'U' in cell_data.keys():
+                unames.add(cell_data['U'])
+        return sorted(list(unames))
+
+    def extract_submodel(self, uname, title=None, take_inserted=True):
+        """Gets the Model instance that corresponds to the specified universe.
+
+        Parameters
+        ----------
+        uname : int
+            The name of universe under consideration.
+        title : str
+            A brief description of the universe.
+        take_inserted : bool
+            Whether to take cells inserted by fill operations. If False, no 
+            filling universes are taken, fill options are moved in comments. 
+            Default True.
+            
+        Returns
+        -------
+        model : Model
+            Model, that corresponds to the specified universe.
+        """
+        if title is None:
+            title = "Universe {0}".format(uname)
+        return Model(title, self.cells, self.surfaces, self.data, uname,
+                     take_inserted=take_inserted)
 
     def universe(self, title=""):
         """Gets the model in object representation.
@@ -352,75 +293,6 @@ class Model:
                 self._transformations[tr] = Transformation(**tr_data)
             return self._transformations[tr]
 
-    def get_universe_list(self):
-        """Gets the list of universe names.
-        
-        Returns
-        -------
-        unames : list[int]
-            List of included universe names.
-        """
-        return list(self.universes.keys())
-
-    def get_universe_model(self, uname, title=None):
-        """Gets the Model instance that corresponds to the specified universe.
-        
-        Parameters
-        ----------
-        uname : int
-            The name of universe under consideration.
-        title : str
-            A brief description of the universe.
-            
-        Returns
-        -------
-        model : Model
-            Model, that corresponds to the specified universe.
-        """
-        if title is None:
-            title = "Universe {0}".format(uname)
-
-        cells = {}
-        for c in self.universes[uname]:
-            pass
-        contained_univ = self.get_contained_universes(uname) | {uname}
-        cells = {}
-        for u in contained_univ:
-            cells.update(self.universes[u])
-        surf_ind = reduce(set.union, [self.get_surface_indices(c['geometry'])
-                                      for c in cells.values()], set())
-        surfaces = {i: self.surfaces[i] for i in surf_ind}
-        tr_ind = set()  # indices of transformations
-        for s in surfaces.values():
-            if 'transform' in s[2].keys():
-                tr_ind.add(s[2]['transform'])
-        mat_ind = set()  # indices of material compositions
-        for c in cells.values():
-            if 'TRCL' in c.keys() and isinstance(c['TRCL'], int):
-                tr_ind.add(c['TRCL'])
-            if 'FILL' in c.keys():
-                if 'transform' in c['FILL'].keys() and \
-                        isinstance(c['FILL']['transform'], int):
-                    tr_ind.add(c['FILL']['transform'])
-            if 'MAT' in c.keys():
-                mat_ind.add(c['MAT'])
-        data = self.data.copy()     # All data cards remain the same except
-                                    # material and transformation cards.
-        data['TR'] = {i: data['TR'][i] for i in tr_ind}
-        data['M'] = {i: data['M'][i] for i in mat_ind}
-        return Model(title, cells, surfaces, data)
-
-    def get_contained_universes(self, uname):
-        """Gets a list of universes, that are contained in this one."""
-        contained = set()
-        for c in self.universes[uname].values():
-            if 'FILL' in c.keys():
-                contained.add(c['FILL']['universe'])
-        inner = []
-        for u in contained:
-            inner.append(self.get_contained_universes(u))
-        return contained.union(*inner)
-
 
 def _get_contained_cells(cells, uname, take_inserted=True):
     """Gets all cells contained in universe uname, directly or indirectly.
@@ -563,27 +435,3 @@ def _get_composition_indices(cells):
         if 'MAT' in cell.keys():
             comps.add(cell['MAT'])
     return comps
-
-        # # --------------------------------------------------------------------------
-        # # Transformation instances creation
-        # if 'TR' in data.keys():
-        #     for tr_name, tr_data in data['TR'].items():
-        #         data['TR'][tr_name] = Transformation(**tr_data)
-        # # Creation of Surface instances
-        # for sur_name, (kind, params, options) in surfaces.items():
-        #     if 'transform' in options.keys():
-        #         tr_name = options['transform']
-        #         options['transform'] = data['TR'][tr_name]
-        #     surfaces[sur_name] = create_surface(kind, *params, **options)
-        # # Creation of Cell instances
-        # # First, we must replace all cell complement operations (NUM followed by#)
-        # # and surface numbers by corresponding Surface instances.
-        # _replace_geometry_names_by_objects(cells, surfaces)
-        #
-        # # Create Material instances and assign them to cells
-        # _create_material_objects(cells, data['M'])
-        #
-        # # Create Cell objects itself.
-        # for cell_name, cell_params in cells.items():
-        #     geometry = cell_params.pop('geometry')
-        #     cells[cell_name] = Cell(geometry, **cell_params)
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,7 @@
 import unittest
 
 from tests.model_test_data.geometry_replace import surf_obj, cell_cases
-from mckit.model import _replace_geometry_names_by_objects, _get_material, \
-    _create_material_objects, _get_universe_dependencies, read_mcnp_model, \
+from mckit.model import _get_universe_dependencies, read_mcnp, \
     _get_surface_indices, _get_contained_cells, _get_composition_indices, \
     _get_transformation_indices, Model
 
@@ -20,61 +19,34 @@ def setUpModule():
             cases[case] = parser.parse(text)
 
 
-# class TestModel(unittest.TestCase):
-#     def test_get_universe_list(self):
-#         for case, data in get_universe_list_ans.items():
-#             model = read_mcnp_model('tests/model_test_data/{0}.txt'.format(case))
-#             self.assertEqual(data, model.get_universe_list())
-#
-#     def test_get_contained_universes_ans(self):
-#         for case, data in contained_universes_ans.items():
-#             model = read_mcnp_model('tests/model_test_data/{0}.txt'.format(case))
-#             for uname, ucont in data.items():
-#                 with self.subTest(i=uname):
-#                     self.assertEqual(model.get_contained_universes(uname), ucont)
-#
-#     def test_get_universe_model(self):
-#         for case, data in get_universe_model_ans.items():
-#             model = read_mcnp_model('tests/model_test_data/{0}.txt'.format(case))
-#             for uname, part_ans in data.items():
-#                 with self.subTest(i=uname):
-#                     part = model.get_universe_model(uname)
-#                     self.assertEqual(part.title, part_ans['title'])
-#                     self.assertEqual(part.cells.keys(), part_ans['cells'])
-#                     self.assertEqual(part.surfaces.keys(), part_ans['surfaces'])
-#                     self.assertEqual(part.data['M'].keys(), part_ans['material'])
-#                     self.assertEqual(part.data['TR'].keys(), part_ans['transform'])
+class TestModel(unittest.TestCase):
+    def test_list_universes(self):
+        for case in case_names:
+            with self.subTest(msg='case: {0}'.format(case)):
+                model = read_mcnp('tests/model_test_data/{0}.txt'.format(case))
+                unames = model.list_universes()
+                self.assertListEqual(unames, model_list_universes_ans[case])
 
+    def test_extract_submodel(self):
+        for case in case_names:
+            model = read_mcnp('tests/model_test_data/{0}.txt'.format(case))
+            for (u, flag), ans in extract_submodel_ans[case].items():
+                msg = "case: {0}, universe: {1}, inserted: {2}".format(case, u, flag)
+                with self.subTest(msg=msg):
+                    submodel = model.extract_submodel(u, take_inserted=flag)
+                    self.assertEqual(submodel.title, ans['title'])
+                    self.assertDictEqual(submodel.cells, ans['cells'])
+                    self.assertDictEqual(submodel.surfaces, ans['surfaces'])
+                    self.assertDictEqual(submodel.data, ans['data'])
 
+    def test_universe(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    
 class TestAuxiliaryFunctions(unittest.TestCase):
-    def test_replace_geometry_names_by_objects(self):
-        for i, cell_dict in enumerate(cell_cases):
-            with self.subTest(i=i):
-                _replace_geometry_names_by_objects(cell_dict, surf_obj)
-                for name, cell in cell_dict.items():
-                    msg = 'cell #{0}'.format(name)
-                    self.assertEqual(cell['geometry'], cell['answer'], msg=msg)
-
-    def test_get_material(self):
-        from tests.model_test_data.material_creation import materials, densities
-        for i, (den, ans) in enumerate(densities):
-            with self.subTest(i=i):
-                mat = _get_material(materials, den)
-                self.assertIs(mat, ans)
-
-    def test_create_material_objects(self):
-        from tests.model_test_data.material_creation import compositions, \
-            cells, mat_cell_ans
-        materials = _create_material_objects(cells, compositions)
-        with self.subTest(msg='Void cells'):
-            for cell_name in mat_cell_ans[0]:
-                self.assertNotIn('material', cells[cell_name].keys())
-        for comp_name, mats in materials.items():
-            with self.subTest(msg="composition = {0}".format(comp_name)):
-                for i, mat in enumerate(mats):
-                    for cn in mat_cell_ans[comp_name][i]:
-                        self.assertIs(cells[cn]['material'], mat)
-
     def test_universe_dependencies(self):
         for case, data in cases.items():
             with self.subTest(msg=case):
@@ -132,6 +104,15 @@ class TestAuxiliaryFunctions(unittest.TestCase):
                     surfaces = {si: data[2][si] for si in sur_ind}
                     output = _get_transformation_indices(new_cells, surfaces)
                     self.assertSetEqual(output, ans)
+
+    def test_read_mcnp(self):
+        for case in case_names:
+            with self.subTest(msg='case: {0}'.format(case)):
+                model = read_mcnp('tests/model_test_data/{0}.txt'.format(case))
+                self.assertEqual(model.title, read_mcnp_ans[case]['title'])
+                self.assertDictEqual(model.cells, read_mcnp_ans[case]['cells'])
+                self.assertDictEqual(model.surfaces, read_mcnp_ans[case]['surfaces'])
+                self.assertDictEqual(model.data, read_mcnp_ans[case]['data'])
 
 
 if __name__ == '__main__':
