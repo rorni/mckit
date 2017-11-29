0700ea05dbabc81dc8f3c851b7e982784d360743
Roman Rodionov <r.rodionov@iterrf.ru>
2017-11-29 16:22:37 +0300

    Adding setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mckit',
+    version='0.1',
+    packages=['mckit'],
+    url='https://gitlab.iterrf.ru/Rodionov/mckit',
+    license='',
+    author='Roma',
+    author_email='r.rodionov@iterrf.ru',
+    description='Tool for handling neutronic models and results'
+)
