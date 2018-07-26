d6219e6e46982be403f905e55ca50542a20cf1b8
Roman Rodionov <rodionov.rn@yandex.ru>
2018-07-26 11:46:18 +0300

    Add autorun capability in documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,12 +31,15 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'numpydoc']
+    'numpydoc',
+    'sphinx_autorun'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
