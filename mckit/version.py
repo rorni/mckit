714d58a3d0997a07cf9939153d603f7b71fdb206
dvp <dmitri_portnov@yahoo.com>
2020-11-08 12:32:42 +0300

    fix: fixed error on reading version from importlib.metadata

--- mckit/version.py
+++ mckit/version.py
@@ -3,12 +3,6 @@ try:
 except ImportError:
     import importlib.metadata as meta
 
-try:
-    __version__ = meta.version(__name__)
-except meta.PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-__version_info__ = tuple(map(int, __version__.split(".")[:3]))
-
 __title__ = "mckit"
 __distribution__ = meta.distribution(__title__)
 __meta_data__ = __distribution__.metadata
@@ -19,3 +13,14 @@ __summary__ = __meta_data__["Summary"]
 __copyright__ = (
     "Copyright 2018-2020 Roman Rodionov"
 )  # TODO dvp: move to meta (project.toml)
+
+__version__ = meta.version("mckit")
+
+try:
+    __version__ = meta.version(__name__)
+except meta.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+__version_info__ = tuple(
+    map(int, __version__.split(".")[:3])
+)  # leaving only the 3 first digits from version
