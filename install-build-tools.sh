32d97b73d80688b1365ad65313f002bc77cd8688
dvp <dmitri_portnov@yahoo.com>
2020-11-10 00:39:15 +0300

    build: added comment to install-build-tools.sh for Windows installation

--- install-build-tools.sh
+++ install-build-tools.sh
@@ -21,10 +21,6 @@ if [[ "$OS" == "Linux" ]]; then
         # This installs the latest poetry for current user global scope.
         # See https://python-poetry.org/docs/#installation#
         #
-        # On Windos use:
-        #  (Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python -
-        #
-        #
         curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
         #
         #    ATTENTION!
@@ -58,6 +54,13 @@ if [[ "$OS" == "Linux" ]]; then
 
 else
     echo "ERROR: Install build tools is not implemented for $OS"
+    echo "       See the comment in ${0}"
+    # There's Windows version for pyenv: https://pypi.org/project/pyenv-win/1.1.2/"
+    # To install poetry on Windos use:
+    #  (Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python -
+    # Then setup PATH: remember pyenv should appear in the PATH before poetry.
+    # In that case poetry will use the local virtual environment created by pyenv.
+    #
 fi
 
 # vim: set ts=4 sw=0: tw=79 ss=0 ft=sh et ai :
