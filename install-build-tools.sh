09994195e79beef196f1d1d5c22e4e1ca6b15895
dvp <dmitri_portnov@yahoo.com>
2020-11-09 16:43:16 +0300

    Implemented install-build-tools.sh to prepare pyenv/poetry/pre-commit tools

---
+++ install-build-tools.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+OS="$(uname)"
+
+if [[ "$OS" == "Linux" ]]; then
+
+    #
+    # install prerequisites
+    #
+    sudo apt update && sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+    libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+    if [[ ! -e pyenv ]]; then
+        #
+        # install pyenv
+        #
+        curl https://pyenv.run | bash
+        # Add the following lines to your ~/.bashrc or ~/.zshrc:
+        #   export PATH="~/.pyenv/bin:$PATH"
+        #   eval "$(pyenv init -)"
+        #   eval "$(pyenv virtualenv-init -)"
+        #
+    fi
+
+    #
+    # upgrade pip
+    #
+    python -m pip install --upgrade pip
+
+    if [[ ! -e poetry ]]; then
+        #
+        # This installs the latest poetry for current user global scope.
+        # See https://python-poetry.org/docs/#installation#
+        #
+        # On Windos use:
+        #  (Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python -
+        #
+        #
+        curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+        # After installation issue the command:
+        #    source ~/.poetry/env
+        # to use it.
+    fi
+
+    #
+    # pre-commit
+:w    # see https://cjolowicz.github.io/posts/hypermodern-python-03-linting/#managing-git-hooks-with-precommit
+    #
+    pip install --upgrade pre-commit
+    pre-commit install
+    pre-commit run -all-files
+
+else
+    echo "ERROR: Install build tools is not implemented for $OS"
+fi
+
+# vim: set ts=4 sw=0: tw=79 ss=0 ft=sh et ai :
