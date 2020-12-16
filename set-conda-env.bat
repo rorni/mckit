e780efb2cff26aa6bd04141884a3d865e24f3275
dvp <dmitri_portnov@yahoo.com>
2020-12-16 18:56:12 +0300

    build: jupyter works from Windows mckit environment

--- set-conda-env.bat
+++ set-conda-env.bat
@@ -4,15 +4,17 @@ call conda create -n mckit4 python=3.8 -q -y
 call conda activate mckit4
 pip install mkl-devel numpy scipy scikit-learn numexpr
 echo The following two commands should point to the same environment
-call conda list
+call conda env list
 call poetry env info --path
 python build_nlopt.py
 call poetry build
 call poetry install
 pytest -m "not slow"
-call poetry run ipython kernel install --user --name=mckit4
+:: tornado (in jupyter) doesn't work with newer version of pywin, check this on jupyter dependencies updates
+call conda install pywin32=227 -y
+call poetry run python -m ipykernel install --user --name mckit4
 mckit --version
 nox --list
-:: nox -s safety
-:: nox -s tests
+nox -s safety
+nox -s tests  -- -m "not slow" --cov
 
