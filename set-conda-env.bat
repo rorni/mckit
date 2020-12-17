7ac18f1ef3a322b6d698863f9ce754d3948f3107
dvp <dmitri_portnov@yahoo.com>
2020-12-17 12:02:03 +0300

    build: add comments to set-conda-env.bat

--- set-conda-env.bat
+++ set-conda-env.bat
@@ -1,20 +1,57 @@
+::
+:: Prepare conda environment for mckit development on Windows.
+::
+:: dvp, Dec 2020
+::
+
 call conda deactivate
 call conda env remove -n mckit4 -q -y
 call conda create -n mckit4 python=3.8 -q -y
 call conda activate mckit4
+
+:: 1) Conda downgrades mkl, so we use pip instead (ant it works fine for us)
+:: 2) We need numpy to build nlopt, other packages are installed just for convenience
 pip install mkl-devel numpy scipy scikit-learn numexpr
-echo The following two commands should point to the same environment
+
+:: The following two commands should point to the same environment
 call conda env list
 call poetry env info --path
+echo .
+echo Check the previous two outputs: do they point to the same environment?
+echo ---------------------------------------------------------------------
+echo Set IDE (VSCode, PyCharm etc.) to use this environment.
+echo .
+
+:: build and install nlopt to the current environment (for developers only, requires CMake)
 python build_nlopt.py
+
+:: build mckit itself
 call poetry build
+
+:: install mckit to the current environment
 call poetry install
+
+:: check if mckit is actually installed and everything is OK with dependencies
+mckit --version
+
+:: verify pytest
 pytest -m "not slow"
+
+:: Jupyter setup
+::
 :: tornado (in jupyter) doesn't work with newer version of pywin, check this on jupyter dependencies updates
+:: TODO dvp: check on dependencies updates
+::
 call conda install pywin32=227 -y
 call poetry run python -m ipykernel install --user --name mckit4
-mckit --version
+
+:: verify nox
 nox --list
 nox -s safety
 nox -s tests  -- -m "not slow" --cov
 
+:: verify if 'pip' is able to collect the dependencies wheels
+:: Note:
+::     'poetry build' (see above) creates the wheel for mckit
+::     So, this command is not necessary, just demo how does 'pip' work without setup.py.
+pip wheel -w dist --verbose .
