f8d3f7e94e17decdf90d2521c666c307fb532aae
dvp <dmitri_portnov@yahoo.com>
2021-01-07 22:45:03 +0300

    fix: load nlopt.dll from package directory first

--- mckit/__init__.py
+++ mckit/__init__.py
@@ -1,4 +1,4 @@
-# flake8: noqa F401
+# flake8: noqa F401, E402
 
 import os
 import platform
@@ -7,17 +7,31 @@ import sys
 from ctypes import cdll
 from pathlib import Path
 
+
+def find_file(_file: str, *directories: Path):
+    """Find a file in directories"""
+    for d in directories:
+        path = d / _file
+        if path.exists():
+            return path
+    raise EnvironmentError(f"Cannot find {_file} in directories {directories}")
+
+
 if platform.system() == "Windows":
-    dll_path = os.path.join(sys.prefix, "Library", "bin")
-    nlopt_lib_path = os.path.join(dll_path, "nlopt.dll")
-    assert os.path.exists(
-       nlopt_lib_path
-    ), f"nlopt.dll should be in ${nlopt_lib_path} before importing mckit"
+    dirs = [
+        Path(__file__).parent,
+        Path(sys.prefix, "Library", "bin"),
+    ]
     if hasattr(os, "add_dll_directory"):  # Python 3.7 doesn't have this method
-        os.add_dll_directory(dll_path)
-    nlopt_lib = cdll.LoadLibrary(nlopt_lib_path)  # to guarantee dll loading 
+        for _dir in dirs:
+            os.add_dll_directory(_dir)
+    dll_path = find_file("nlopt.dll", *dirs)
+    nlopt_lib_path = os.path.join(dll_path, "nlopt.dll")
+    cdll.LoadLibrary(nlopt_lib_path)  # to guarantee dll loading
 else:
-    if os.environ.get("LD_LIBRARY_PATH") is None:  # a user can use other location form mkl library.
+    if (
+        os.environ.get("LD_LIBRARY_PATH") is None
+    ):  # a user can use other location form mkl library.
         # preload library
         mkl_lib_path = Path(sys.prefix, "lib", "libmkl_rt.so.1")
         assert (
