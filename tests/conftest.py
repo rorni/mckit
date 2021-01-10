5ada0e39c354a4ecfa1d9c09f01f5e72f337f412
dvp <dmitri_portnov@yahoo.com>
2021-01-11 01:18:56 +0300

    style: TODO is removed as implemented in other code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,11 @@
-# tests/conftest.py
 from _pytest.config import Config
 
-# TODO dvp: the following doesn't work, check why?
-
-# import os.path
-# import sys
-# from pathlib import Path
-#
-# import pytest
-#
-# PREFIX_PATH = Path(sys.prefix)
-#
-#
-# @pytest.fixture(scope="session", autouse=True)
-# def set_ld_library_path(request):
-#     print("Setting LD_LIBRARY_PATH")
-#     old_ld_library_path = os.getenv("LD_LIBRARY_PATH", "").split(os.pathsep)
-#     lib_path = PREFIX_PATH / "lib"
-#     assert lib_path.is_dir()
-#     new_ld_library_path = str(lib_path.resolve())
-#
-#     if new_ld_library_path not in old_ld_library_path:
-#
-#         if old_ld_library_path:
-#             new_ld_library_path = os.path.pathsep.join([new_ld_library_path, *old_ld_library_path])
-#             os.environ["LD_LIBRARY_PATH"] = new_ld_library_path
-#
-#             def finalizer():
-#                 os.environ["LD_LIBRARY_PATH"] = os.pathsep.join(old_ld_library_path)
-#         else:
-#             def finalizer():
-#                 pass
-#
-#         request.addfinaYieldGeneratorlizer(finalizer)
+markers = [
+    'slow: marks tests as slow (deselect with -m "not slow"',
+    # "e2e: mark as end-to-end test."
+]
 
 
 def pytest_configure(config: Config) -> None:
-    config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
-    config.addinivalue_line(
-        "markers", 'slow: marks tests as slow (deselect with -m "not slow")'
-    )
+    for m in markers:
+        config.addinivalue_line("markers", m)
