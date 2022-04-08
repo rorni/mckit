4f4ef5c68db55f75f492c0aa42da9e9e9960625a
dvp <dmitri_portnov@yahoo.com>
2022-04-08 14:48:56 +0300

    fix: correct mkl_rt library name in Windows package

--- mckit/_init_dynamic_libraries.py
+++ mckit/_init_dynamic_libraries.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Generator, List
 
 import os
 import sys
@@ -19,49 +19,68 @@ def library_base_name(_lib_name: str) -> str:
     return _lib_name if WIN else "lib" + _lib_name
 
 
-SUFFIXES = (
-    [".dll"]
-    if WIN
-    else ".2.dylib .1.dylib .0.dylib .dylib".split()
-    if MACOS
-    else ".so.2 .so.1 .so.0 .so".split()
-)
+SUFFIX = ".dll" if WIN else ".dylib" if MACOS else ".so"
+
+if WIN or MACOS:
+
+    def combine_version_and_suffix(version: int, suffix: str) -> str:
+        return f"{version}.{suffix}"  # .2.dll or .2.dylib
+
+else:  # Linux
+
+    def combine_version_and_suffix(version: int, suffix: str) -> str:
+        return f"{suffix}.{version}"  # .so.2
 
 
-LIBRARY_DIRECTORIES: List[Path] = []
+def iterate_suffixes_with_version(max_version: int = 2) -> Generator["str", None, None]:
+    while max_version >= 0:
+        yield combine_version_and_suffix(max_version, SUFFIX)
+        --max_version
+    yield SUFFIX
+
+
+# SUFFIXES = (
+#     [".dll"]
+#     if WIN
+#     else ".2.dylib .1.dylib .0.dylib .dylib".split()
+#     if MACOS
+#     else ".so.2 .so.1 .so.0 .so".split()
+# )
+
+
+SHARED_LIBRARY_DIRECTORIES: List[Path] = []
 
 if WIN:
-    LIBRARY_DIRECTORIES.append(Path(sys.prefix, "Library", "lib"))
+    SHARED_LIBRARY_DIRECTORIES.append(Path(sys.prefix, "Library", "bin"))
 else:
     ld_library_path = os.environ.get(
         "DYLD_LIBRARY_PATH" if MACOS else "LD_LIBRARY_PATH"
     )
     if ld_library_path:
-        LIBRARY_DIRECTORIES.extend(map(Path, ld_library_path.split(":")))
-    LIBRARY_DIRECTORIES.append(Path(sys.prefix, "lib"))
+        SHARED_LIBRARY_DIRECTORIES.extend(map(Path, ld_library_path.split(":")))
+    SHARED_LIBRARY_DIRECTORIES.append(Path(sys.prefix, "lib"))
 
-LIBRARY_DIRECTORIES.append(HERE)
+SHARED_LIBRARY_DIRECTORIES.append(HERE)
 
 
-def preload_library(_lib_name: str) -> bool:
-    for d in LIBRARY_DIRECTORIES:
-        for s in SUFFIXES:
-            p = Path(d, library_base_name(_lib_name)).with_suffix(s)
+def preload_library(lib_name: str, max_version: int = 2) -> None:
+    for d in SHARED_LIBRARY_DIRECTORIES:
+        for s in iterate_suffixes_with_version(max_version):
+            p = Path(d, library_base_name(lib_name)).with_suffix(s)
             if p.exists():
                 cdll.LoadLibrary(str(p))
                 _LOG.info("Found library: {}", p.absolute())
-                return True
-    return False
+                return
+    raise EnvironmentError(f"Cannot preload library {lib_name}")
 
 
 def init():
     if WIN:
         if hasattr(os, "add_dll_directory"):  # Python 3.7 doesn't have this method
-            for _dir in LIBRARY_DIRECTORIES:
+            for _dir in SHARED_LIBRARY_DIRECTORIES:
                 os.add_dll_directory(str(_dir))
-    for lib_name in ["mkl_rt", "nlopt"]:
-        if not preload_library(lib_name):
-            raise EnvironmentError(f"Cannot preload library {lib_name}")
+    preload_library("mkl_rt", max_version=2)
+    preload_library("nlopt", max_version=0)
 
 
 init()
