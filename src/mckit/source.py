ef7fd04ee35c0c83b31fe86809d6b1a8f40f4a34
dvp <d.portnov@iterrf.ru>
2023-01-25 18:30:41 +0300

    test: increase coverage in source.py

--- src/mckit/source.py
+++ src/mckit/source.py
@@ -1,11 +1,11 @@
 """Code to load source distribution and print corresponding MCNP SDEF."""
 from __future__ import annotations
 
-from typing import Dict, List, Optional, Set, Tuple, Union
-
-from functools import reduce
+from typing import Dict, Optional, Set, Tuple, Union
 
 import numpy as np
+
+from numpy.typing import ArrayLike
 
 from .printer import print_card, separate
 
@@ -18,66 +18,47 @@ class Distribution:
     probability density values. If len(probs) + 1 == len(values), then
     values are bin boundaries for continuous distribution.
 
-    Parameters
-    ----------
-    name : int
-        Distribution's name. It is needed for MCNP.
-    values : list
-        A list of values, that variable can be equal to. Values can be a float,
-        an int or other Distribution instances.
-    probs : list or Distribution
-        A list of probabilities or a distribution this one depends on.
-        Probabilities need not be normalized.
-    variable : str
-        Source variable's name. Default: None.
-    is_discrete : bool
-        Indicate that the variable is discrete. Default: False.
+    Args:
+        name:  Distribution's name. It is needed for MCNP.
+        values: A list of values, that variable can be equal to. Values can be a float,
+                an int or other Distribution instances.
+        probs: A list of probabilities or a distribution this one depends on.
+               Probabilities need not be normalized.
+        distribution_variable:   Source variable's name. Default: None.
+        is_discrete: Indicate that the variable is discrete. Default: False.
 
-    Methods
-    -------
-    mcnp_repr()
-        Returns a string representation of corresponding MCNP card.
-    get_inner()
-        Gets all distributions this distribution depends on.
-    depends_on()
-        Gets distribution this one depends on.
-
-    Properties
-    ----------
-    name : int
-        Distribution's name.
-    size : int
-        Size of the distribution. It is the number of values or bins.
-    variable : str
-        Name of source variable.
+    Attributes:
+        _name: Distribution's name
+        _var: Name of source variable
+        _is_discrete: is this discrete or histogram distribution
+        _values: bin values
+        _probs: probabilities or distributions for bins
+        _is_pdf: is this PDF or more complicated distribution?
     """
 
     def __init__(
         self,
         name: int,
-        values: list[Union[int, float, "Distribution"]],
-        probs: Union[List[float], "Distribution"],
-        variable: Optional[str] = None,
+        values: [Union[ArrayLike, list["Distribution"]]],
+        probs: Union[ArrayLike, "Distribution"],
+        distribution_variable: Optional[str] = None,
         is_discrete: bool = False,
     ) -> None:
         self._name = name
-        self._var = variable
+        self._var = distribution_variable
         Distribution.check_distr(values, len(probs), is_discrete)
         self._is_discrete = is_discrete
-        self._values = list(values)
+        self._values = np.asarray(values)
         if isinstance(probs, Distribution):
-            self._probs: Union[List[float], "Distribution"] = probs
+            self._probs: Union[ArrayLike, "Distribution"] = probs
         else:
-            self._probs = list(probs)
-        if (
-            isinstance(self._probs, list)
+            self._probs = np.asarray(probs)
+        self._is_pdf = (
+            isinstance(self._probs, np.ndarray)
             and len(self._probs) == len(values)
             and not is_discrete
             and not isinstance(self._values[0], Distribution)
-        ):
-            self._is_pdf = True
-        else:
-            self._is_pdf = False
+        )
 
     @property
     def name(self) -> int:
@@ -86,7 +67,11 @@ class Distribution:
 
     @property
     def size(self) -> int:
-        """Gets distribution's size."""
+        """The distribution's size.
+
+        Returns:
+            The number of probability values.
+        """
         return len(self._probs)
 
     @property
@@ -94,44 +79,42 @@ class Distribution:
         """Gets distribution's source variable name."""
         return self._var
 
+    @property
+    def has_nested_distributions(self) -> bool:
+        """Are there nested (inner) distributions?"""
+        return isinstance(self._values[0], Distribution)
+
+    @staticmethod
+    def check_distr(
+        bins_or_distrs: [Union[ArrayLike, list["Distribution"]]], size: int, is_discrete: bool
+    ) -> None:
+        """Checks if the distribution is correct.
+
+        Args:
+            bins_or_distrs: List of variable values.
+            size: The length of intensity matrix along variable dimension.
+            is_discrete:  True <=> the distribution is discrete.
+
+        Raises:
+            ValueError: if the distribution is neither discrete, nor pdf, nor histogram.
+        """
+        discrete_or_pdf = len(bins_or_distrs) == size
+        histogram = len(bins_or_distrs) == size + 1 and not is_discrete
+        if not discrete_or_pdf and not histogram:
+            raise ValueError("Inconsistent size of values.")
+        _check_all_are_distributions_or_not(bins_or_distrs)
+
     def mcnp_repr(self) -> str:
         """Returns a string representation of corresponding MCNP card."""
-        tokens = []
-        discrete = self._is_discrete
-        is_pdf = self._is_pdf
-        if isinstance(self._values[0], Distribution):
-            tokens.append("S")
-            for v in self._values:
-                tokens.append(str(v.name))
-        else:
-            if isinstance(self._values[0], list):
-                values = reduce(list.__add__, self._values)
-            else:
-                values = self._values
-            if discrete:
-                tokens.append("L")
-            elif is_pdf:
-                tokens.append("A")
-            else:
-                tokens.append("H")
-            for v in values:
-                tokens.append(str(v))
+        bin_tokens = self._bin_tokens_repr()
 
-        if isinstance(self._probs, Distribution):
-            tokens.insert(0, f"DS{self._name}")
-            prob_tokens = None
-        else:
-            tokens.insert(0, f"SI{self._name}")
-            prob_tokens = [f"SP{self._name}"]
-            if not is_pdf:
-                prob_tokens.append("D")
-            if len(self._values) == len(self._probs) + 1:
-                prob_tokens.append("0")
-            for p in self._probs:
-                prob_tokens.append(str(p))
-        card = print_card(separate(tokens))
+        prob_tokens = self._prob_tokens_repr(bin_tokens)
+
+        card = print_card(separate(bin_tokens))
+
         if prob_tokens:
             card += "\n" + print_card(separate(prob_tokens))
+
         return card
 
     def get_inner(self) -> Set["Distribution"]:
@@ -141,22 +124,9 @@ class Distribution:
         then they are returned.
 
         Returns:
-            Set:  A set of nested distributions.
-
-        Raises:
-            TypeError: when one of the values is not a `Distribution`.
+            Set:  A set of nested distributions, if any, empty set otherwise.
         """
-        dists: Set["Distribution"] = set()
-        if isinstance(self._values[0], Distribution):
-            for v in self._values:
-                if not isinstance(v, Distribution):
-                    raise TypeError(f"Expect `Distribution` type, actual {type(v)}")
-                dists.add(v)
-        return dists
-
-    def __len__(self) -> int:
-        """Size of distribution."""
-        return len(self._probs)
+        return set(self._values) if self.has_nested_distributions else set()
 
     def depends_on(self) -> Optional["Distribution"]:
         """Gets distribution this one depends on.
@@ -169,24 +139,56 @@ class Distribution:
         else:
             return None
 
-    @staticmethod
-    def check_distr(
-        values: list[Union[int, float, "Distribution"]], size: int, is_discrete: bool
-    ) -> None:
-        """Checks if the distribution is correct.
+    def _mcnp_distribution_tag(self):
+        if self._is_discrete:
+            return "L"
+        elif self._is_pdf:
+            return "A"
+        else:
+            return "H"
 
-        Args:
-            values: List of variable values.
-            size: The length of intensity matrix along variable dimension.
-            is_discrete:  True <=> the distribution is discrete.
+    def _prob_tokens_repr(self, bin_tokens):
+        if isinstance(self._probs, Distribution):
+            bin_tokens.insert(0, f"DS{self._name}")
+            prob_tokens = None
+        else:
+            bin_tokens.insert(0, f"SI{self._name}")
+            prob_tokens = [f"SP{self._name}"]
 
-        Raises:
-            ValueError: if the distribution is neither discrete, nor pdf, nor histogram.
-        """
-        discr_or_pdf = len(values) == size
-        histogram = len(values) == size + 1 and not is_discrete
-        if not discr_or_pdf and not histogram:
-            raise ValueError("Inconsistent size of values.")
+            if not self._is_pdf:
+                prob_tokens.append("D")
+
+            if len(self._values) == len(self._probs) + 1:
+                prob_tokens.append("0")
+
+            prob_tokens.extend(str(p) for p in self._probs)
+        return prob_tokens
+
+    def _bin_tokens_repr(self):
+        bin_tokens = []
+        if self.has_nested_distributions:
+            bin_tokens.append("S")
+            bin_tokens.extend(str(v.name) for v in self._values)
+        else:
+            bin_tokens.append(self._mcnp_distribution_tag())
+
+            if isinstance(self._values[0], np.ndarray):
+                values_repr = np.concatenate(self._values)
+            else:
+                values_repr = self._values
+
+            bin_tokens.extend(str(v) for v in values_repr)
+        return bin_tokens
+
+    def __len__(self) -> int:
+        """Size of distribution."""
+        return len(self._probs)
+
+
+def _check_all_are_distributions_or_not(bins_or_distrs):
+    expect_distributions = isinstance(bins_or_distrs[0], Distribution)
+    if not all(expect_distributions == isinstance(x, Distribution) for x in bins_or_distrs):
+        raise TypeError("Distribution bins should be either all Distributions or all not")
 
 
 def create_bin_distributions(
@@ -211,62 +213,13 @@ def create_bin_distributions(
     return start_name, distributions
 
 
-def expand_matrix_distribution(
-    intensities: np.ndarray,
-    *var_values: list[float],
-    start_name: int = 1,
-) -> Tuple[int, Tuple[list[float], ...], List[float]]:
-    """Converts matrix distribution to the len(var_values) linear.
-
-    Creates:
-        - expr_valu_values -  A tuple of lists of values for each source variable.
-        - exp_intensities -   A list of expanded intensities for every hyperbin.
-
-    Args:
-        intensities :  A matrix of source intensities.
-        var_values :   A tuple of variable values along each axis.
-        start_name :   Starting name for distributions. Default: 1.
-
-    Returns:
-        free_name: int,  exp_var_values : tuple, exp_intensities : list[float]
-
-    Raises:
-        ValueError: if the distribution is neither discrete, nor pdf, nor histogram.
-    """
-    if len(intensities.shape) != len(var_values):
-        msg = "Length of var_values must be equal to the number of intensities dimensions."
-        raise ValueError(msg)
-    uniq_values = []
-    exp_var_values = []
-    for dim, values in zip(intensities.shape, var_values):
-        if Distribution.check_distr(values, dim):
-            uniq_values.append(values)
-        else:
-            start_name, dists = create_bin_distributions(values, start_name)
-            uniq_values.append(dists)
-        exp_var_values.append([])
-
-    exp_intensities = []
-    for index, intensity in intensities:
-        for j, i in enumerate(index):
-            exp_var_values[j].append(uniq_values[j][i])
-        exp_intensities.append(intensity)
-    return start_name, tuple(exp_var_values), exp_intensities
-
-
 class Source:
     """Represents particle source.
 
-    Parameters
-    ----------
-    variables : dict
-        A dictionary of source variables. var_name -> var_value. The value
-        can be either float (int) value or Distribution instance.
-
-    Methods
-    -------
-    mcnp_repr()
-        Gets a string representation of corresponding MCNP card.
+    Args:
+        variables:
+            A dictionary of source variables. var_name -> var_value. The value
+            can be either float (int) value or Distribution instance.
     """
 
     def __init__(self, **variables: Dict[int, Union[int, float, Distribution]]) -> None:
@@ -278,16 +231,6 @@ class Source:
         cards = []
         extra_cards = []
 
-        def _var_repr(value) -> str:
-            if isinstance(value, Distribution):
-                dep = value.depends_on()
-                result = f"D{value.name}"
-                if dep:
-                    result = f"F{dep.variable} " + result
-            else:
-                result = str(value)
-            return result
-
         for k, v in self._variables.items():
             tokens.append(f"{k}={_var_repr(v)}")
             if isinstance(v, Distribution):
@@ -297,3 +240,14 @@ class Source:
         cards.insert(0, print_card(separate(tokens)))
         cards.extend(extra_cards)
         return "\n".join(cards)
+
+
+def _var_repr(distribution) -> str:
+    if isinstance(distribution, Distribution):
+        dep = distribution.depends_on()
+        text = f"D{distribution.name}"
+        if dep:
+            text = f"F{dep.variable} " + text
+    else:
+        text = str(distribution)
+    return text
