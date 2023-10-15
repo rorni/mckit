e2af6b3b2c3e2f54ac01658592eb1e8ae08bc80d
dvp <d.portnov@iterrf.ru>
2023-10-15 14:10:36 +0300

    feat(adhoc): verify volumes in EP11 model with Co05

--- adhoc/ep11/verify_co05_volumes.py
+++ adhoc/ep11/verify_co05_volumes.py
@@ -2,7 +2,8 @@
 """Find ratio of a volume fo steel with Co0.5 fraction to other steel volume.
 
 The model pc11_shields8_Co05.i is variant where the specific material was applied for
-frame parts, which are in the integrator scope - fraction of Co was increased to 0.5wt%.
+frame parts, which are in the integrator scope - fraction of Co was increased to 0.5%.
+The material's id is 207302.
 All other parts (diagnostics) use the same materials as in baseline model.
 
 We want to show that the portion of the specific steel is much less than of other steel
@@ -18,17 +19,31 @@ import numpy as np
 
 import dotenv
 
-from mckit.geometry import Box
+from mckit.geometry import EX, EY, EZ, Box
 from mckit.parser import from_file
 from tqdm import tqdm
 
 dotenv.load_dotenv()
 
-MODEL_PATH = Path(os.getenv("MCKIT_MODEL", "~/dev/mcnp/ep11/wrk/pc/Co05-volumes")).expanduser()
-if not MODEL_PATH.exists():
+WRK_DIR = Path(os.getenv("MCKIT_MODEL", "~/dev/mcnp/ep11/wrk/pc/Co05-volumes")).expanduser()
+if not WRK_DIR.is_dir():
+    raise FileNotFoundError(WRK_DIR)
+
+MODEL_PATH = WRK_DIR / "pc11_shields8_Co05.i"
+if not MODEL_PATH.is_file():
     raise FileNotFoundError(MODEL_PATH)
 
-WRK_DIR = MODEL_PATH.parent
+
+def from_bounds(
+    minx: float, maxx: float, miny: float, maxy: float, minz: float, maxz: float
+) -> Box:
+    min_vals = np.array([minx, miny, minz])
+    max_vals = np.array([maxx, maxy, maxz])
+    if not np.all(min_vals < max_vals):
+        raise ValueError("Unsorted boundaries values")
+    center = 0.5 * (min_vals + max_vals)
+    widths = max_vals - min_vals
+    return Box(center, *widths, ex=EX, ey=EY, ez=EZ)
 
 
 def create_global_box():
@@ -44,7 +59,19 @@ def create_global_box():
     206004 PZ -620
     206005 PZ 740
     """
-    return Box.from_bounds()
+    return from_bounds(830.0, 3350, -1200.0, 1200.0, -620, 740)
+
+
+GLOBAL_BOX = create_global_box()
+
+
+model_info = from_file(MODEL_PATH)
+cells_index = model_info.cells_index
+model = model_info.universe
+
+
+def collect_cell_materials_map(model):
+    pass
 
 
 def main():
